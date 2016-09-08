a76bbb676e18ecab779af9836df3f9d46fff8354
martreba <martreba@greeny.cs.tlu.ee>
2016-09-08 15:16:49 +0300

    Added my first lines of code

---
+++ first.php
@@ -0,0 +1,11 @@
+<?php
+	$firstname = "Martin";
+	$lastname = "Rebane";
+
+	// käsk echo trükib välja muutuja väärtuse
+	//echo $firstname;
+	//echo $lastname;
+	
+	echo $firstname." " .$lastname
+
+?>
